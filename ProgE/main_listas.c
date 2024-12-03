7cb3c11aa57c7a803c03e97722700ea48b4ddcb5
Yuri Abitbol De Menezes Frota <52009988+yuri-frota@users.noreply.github.com>
2024-12-03 08:47:29 -0300

    Add files via upload

---
+++ ProgE/main_listas.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include"tad_listas.c"
+
+int main()
+{
+    int k;
+
+    lista *L = NULL;
+    L = insere_lista(L, 5);
+    L = insere_lista(L, 4);
+    L = insere_lista(L, 3);
+    L = insere_lista(L, 4);
+    L = insere_lista(L, 5);
+    imprime_lista(L);
+
+    L = exclui_lista (L);
+
+    return 0;
+}
+
